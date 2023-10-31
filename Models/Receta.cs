2421f55fb00012408ba127b17ae6e323010c2c47
YAEL SAROKA <47431986@ort>
2023-10-31 11:40:48 -0300

    modals terminados

---
+++ Models/Receta.cs
@@ -0,0 +1,14 @@
+public class Receta{
+    public int IdReceta{get;set;}
+    public int IdRecetaxIngrediente{get;set;}
+    public int IdUsuario{get;set;}
+    public int IsCategoria{get;set;}
+    public string Imagen{get;set;}
+    public string Titulo {get;set;}
+    public string Descripcion{get;set;}
+    public string Pasos{get;set;}
+    public int CantidadPersonas{get;set;}
+    public string Tiempo{get;set;}
+    public float Precio{get;set;}
+    public string Video {get;set;}
+}
